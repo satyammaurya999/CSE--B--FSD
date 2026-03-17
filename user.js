ae4077fca324ea89294ab3733066715527a5cf71
SATYAM MAURYA <148178604+satyammaurya999@users.noreply.github.com>
2026-03-17 16:18:47 +0530

    Create user.js

---
+++ user.js
@@ -0,0 +1,11 @@
+// Dummy database
+
+const users = [
+    {
+        id: 1,
+        username: "admin",
+        password: "$2b$10$wH6QZ6yTQ8v5YFQJ8z8vUuWQx1l6r4X2g9b9R7qYyZq3gQ6z1Yf2K"
+    }
+];
+
+module.exports = users;
